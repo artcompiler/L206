f084bd55ab07db3c8de4d6567ed1f62682156892
Jeff Dyer <jeffdyer@acm.org>
2015-10-31 12:58:36 -0600

    Fix build process

--- src/viewer.js
+++ src/viewer.js
@@ -1,7 +1,7 @@
 /* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 /* Copyright (c) 2015, Jeff Dyer, Art Compiler LLC */
-import {assert, message, messages, reserveCodeRange} from "./assert";
+import {assert, message, messages, reserveCodeRange} from "./assert.js";
 window.exports.viewer = (function () {
   function update(el, obj, src, pool) {
     var data = JSON.parse(obj).data;
